3ec393bf3e6c2d3a56bb6b51ca8271b048df4d02
Debasish <debasish7044200115@gmail.com>
2021-04-17 14:10:16 +0530

    changed

--- resources/views/welcome/index.blade.php
+++ resources/views/welcome/index.blade.php
@@ -39,7 +39,7 @@
               @foreach ($posts as  $post)
                   <div class="col-md-6">
                   <div class="card border hover-shadow-6 mb-6 d-block">
-                    <a href="{{ route('blog.show', $post->id) }}"><img class="card-img-top" src="{{ asset('storage/'.$post->image) }}" style=" height:13rem" alt="Card image cap"></a>
+                    <a href="{{ route('blog.show', $post->id) }}"><img class="card-img-top" src="{{ secure_asset('storage/'.$post->image) }}" style=" height:13rem" alt="Card image cap"></a>
                     <div class="p-6 text-center">
                       <p><a class="small-5 text-lighter text-uppercase ls-2 fw-400" href="#">{{ $post->category->name }}</a></p>
                       <h5 class="mb-0"><a class="text-dark" href="{{ route('blog.show', $post->id) }}">{{ $post->title }}</a></h5>
